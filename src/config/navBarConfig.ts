88a3fb45307e134e4a481a6020b1294c759ddc22
PEYOITE <peyoite233@outlook.com>
2026-05-30 08:46:06 +0800

    更新内容

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -71,14 +71,8 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 				icon: "fa7-brands:github",
 			},
 			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
 				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
+				url: "https://qm.qq.com/q/QZnxXHejwm",
 				external: true,
 				icon: "fa7-brands:qq",
 			},
